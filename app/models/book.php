7eb3b14fc5f1bf2828de3df183965c7a1aebdaec
Cedric Catoul <catoul.cedric@protonmail.com>
2025-11-25 15:54:39 +0100

    remontée du getter magique

--- app/models/book.php
+++ app/models/book.php
@@ -2,32 +2,11 @@
 
 namespace App\Models;
 
-class Book{
-    public $id, $isbn,$title, $resume, $cover,
-    $created_at, $author_id, $category_id;
+class Book extends \Core\Model{ // avec ça il va récupérer ce qui lui est utile dans le dossier pour fonctionner
+    public $isbn,$title, $resume, $cover,
+    $author_id, $category_id;
     // Liaisons
-    private $author, $category;
+    protected $author, $category;
 
-    public function __get(string $prop): mixed
-    {
-        // 1.si le setter existe
-             $setterName= "set" . ucfirst($prop); //$setterName= setXxxx
-             if(method_exists($this, $setterName)):
-                $this->$setterName(); //
-                return $this->$prop; //retourne le mots demander
-             endif;
-             return true;
-    }
-    public function setAuthor()
-    {
-        if($this->author): //($this->author==null)
-        $this->author = AuthorsRepository::findOneById($this->author_id);
-        endif;
-    }
-    public function setCategory()
-    {
-        if($this->category):
-        $this->category = CategoriesRepository::findOneById($this->category_id);
-        endif;
-    }
+   //ici viennent les setter mis dans \Core\Model
 }
