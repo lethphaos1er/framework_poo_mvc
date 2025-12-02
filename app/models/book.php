a6f103e11e0b3a63801d1097639e5229dc8d385a
Cedric Catoul <catoul.cedric@protonmail.com>
2025-12-02 08:55:17 +0100

    modèles générique
    lazy 1-n

--- app/models/book.php
+++ app/models/book.php
@@ -10,3 +10,12 @@ class Book extends \Core\Model{ // avec ça il va récupérer ce qui lui est uti
 
    //ici viennent les setter mis dans \Core\Model
 }
+// echo $book->title; // pour afficher le titre d'un livre
+
+// echo $book->author->>firstname; // pour afficher le prénom de l'auteur du livre
+
+//foreach $author->books (1-n à l'envers)
+
+//foreach $book ->tags (n-m)
+
+//foreach $tag->books (n-m à l'envers)
